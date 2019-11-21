3086daedbb7cd4034478c6dae3e319ab3b2bdd0a
liujian4 <lj130150@sina.com>
2019-11-21 14:55:38 +0800

    修复java.util.regex.Pattern.matches接口使用未取反导致结果错误问题

--- src/main/java/com/bigboss/compiler/check/name/NameChecker.java
+++ src/main/java/com/bigboss/compiler/check/name/NameChecker.java
@@ -60,7 +60,7 @@ public class NameChecker extends ElementScanner8 {
     @Override
     public Object visitType(TypeElement e, Object o) {
         Name name = e.getSimpleName();
-        if(Pattern.matches(TYPE_PATTERN, name)){
+        if(!Pattern.matches(TYPE_PATTERN, name)){
             messager.printMessage(Diagnostic.Kind.WARNING,"类、接口、枚举名称"+name+"应符合驼峰式命名法且首字母大写-[正则:"+TYPE_PATTERN+"]",e);
         }
         return super.visitType(e, o);
@@ -79,11 +79,11 @@ public class NameChecker extends ElementScanner8 {
         Name name = e.getSimpleName();
         //如果为常量则按照大写字母规范检查，否则按照驼峰命名法检查
         if(null != e.getConstantValue()){
-            if(Pattern.matches(CONSTANT_PATTERN, name)){
+            if(!Pattern.matches(CONSTANT_PATTERN, name)){
                 messager.printMessage(Diagnostic.Kind.WARNING,"常量名称"+name+"应所有字母大写且以下划线连接-[正则:"+CONSTANT_PATTERN+"]",e);
             }
         }else {
-            if(Pattern.matches(VARIABLE_OR_PARAMETER_PATTERN, name)){
+            if(!Pattern.matches(VARIABLE_OR_PARAMETER_PATTERN, name)){
                 messager.printMessage(Diagnostic.Kind.WARNING,"变量名称"+name+"应符合驼峰式命名法且首字母小写-[正则:"+VARIABLE_OR_PARAMETER_PATTERN+"]",e);
             }
         }
@@ -100,7 +100,7 @@ public class NameChecker extends ElementScanner8 {
     @Override
     public Object visitExecutable(ExecutableElement e, Object o) {
         Name name = e.getSimpleName();
-        if(Pattern.matches(VARIABLE_OR_PARAMETER_PATTERN, name)){
+        if(!Pattern.matches(VARIABLE_OR_PARAMETER_PATTERN, name)){
             messager.printMessage(Diagnostic.Kind.WARNING,"方法名称"+name+"应符合驼峰式命名法且首字母小写-[正则:"+VARIABLE_OR_PARAMETER_PATTERN+"]",e);
         }
         return super.visitExecutable(e, o);
@@ -116,7 +116,7 @@ public class NameChecker extends ElementScanner8 {
     @Override
     public Object visitTypeParameter(TypeParameterElement e, Object o) {
         Name name = e.getSimpleName();
-        if(Pattern.matches(VARIABLE_OR_PARAMETER_PATTERN, name)){
+        if(!Pattern.matches(VARIABLE_OR_PARAMETER_PATTERN, name)){
             messager.printMessage(Diagnostic.Kind.WARNING,"参数名称"+name+"应符合驼峰式命名法且首字母小写-[正则:"+VARIABLE_OR_PARAMETER_PATTERN+"]",e);
         }
         return super.visitTypeParameter(e, o);
@@ -132,7 +132,7 @@ public class NameChecker extends ElementScanner8 {
     @Override
     public Object visitPackage(PackageElement e, Object o) {
         Name name = e.getSimpleName();
-        if(Pattern.matches(PACKAGE_PATTERN, name)){
+        if(!Pattern.matches(PACKAGE_PATTERN, name)){
             messager.printMessage(Diagnostic.Kind.WARNING,"包路径"+name+"应所有字母小写且以.连接-[正则:"+PACKAGE_PATTERN+"]",e);
         }
         return super.visitPackage(e, o);
